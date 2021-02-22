d2c37dbbf96185cd2e82985e6178f2c2437a9765
jeremy barbosa <jeremybarbosa@jeremys-mbp.home>
2021-02-22 14:28:01 -0600

    f fizzy

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
+function fizzy(num){
+  if (num % 3 === 0 && num % 5 ===0){
+    return "FizzBuzz";
+  }
+  else if(num % 3 === 0){
+    return "Fizz";
+  }
+  else if(num % 5 === 0){
+    return "Buzz";
+  }
+  else {
+    return num;
+  }
+}
 
-
-
-
+// test1 = fizBuz(21);
+// test1;
+// test2 = fizBuz(30);
+// test2;
+// test3 = fizBuz(4);
+// test3;
 
 
 
